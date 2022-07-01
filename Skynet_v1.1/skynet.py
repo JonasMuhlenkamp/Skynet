d1c551473f9da71376a3a1998dcbf569090178c5
JonasMuhlenkamp <jonasmuhlenkamp@gmail.com>
2022-07-01 03:03:19 -0400

    Code is fully functional save testing

--- Skynet_v1.1/skynet.py
+++ Skynet_v1.1/skynet.py
@@ -1,30 +1,14 @@
 import xlwings as xw
 import pandas
 from datetime import datetime
-from card_pricer import download_bulk_data, get_tcg_pricing
+from skynet_pricer import download_bulk_data, get_tcg_pricing
 import win32api
 
-# cards, card_count = download_bulk_data()
-# prices = get_tcg_pricing(cards, card_count)
-
-# cards_df = pandas.DataFrame.from_dict(cards, orient="index")
-# cards_df = cards_df.sort_values(by=['cardname', 'set_name', 'style_type', 'foil_type'])
-# print(cards_df)
-
-# prices_df = pandas.DataFrame.from_dict(prices, orient="index")
-# prices_df = prices_df.sort_index()
-# print(prices_df)
-
-app = xw.apps.active
-wb = app.books.active
-
-# xw.Range("Cards").clear()
-# xw.Range("Cards").value = cards_df
-
-# xw.Range("CurrentPrices").clear()
-# xw.Range("CurrentPrices").value = prices_df
-# wb.sheets["Pricing"].range("A1").value = "6/29/2022"
-
+'''
+These methods are meant to be called from within macro-activated functions.
+They do not establish the active workbook on their own; this should be 
+communicated via the wb and sheet parameters. 
+'''
 def last_row(sheet):
     '''Returns the number of the last row with data in a sheet.'''
     try:
@@ -69,14 +53,21 @@ def build_master_name(input_card):
     
     return master_name 
 
-def add_cards(sheet):
+def add_cards(sheet, wb):
+    '''Adds cards from the entry field into a given sheet.
+    sheet: the wb.sheets["name"] sheet that will be manipulated
+    wb: the workbook object
+    '''
+    
+    # check the entry field for errors and warn the user
     error_check = wb.sheets["Input"].range("f9").value.split(" ")[2].strip()
     if int(error_check) > 0:
         win32api.MessageBox(wb.app.hwnd, "Errors have been detected in your input! Please check that the card is spelled correctly and that you have assembled a correct " + 
             "combination of set, style, and foiling.\n\nYou can use the Card Search function for additional help.", "Input Error Detected")
         return
     
-    last_input_row = min(250, wb.sheets["Input"].range("B" + str(wb.sheets["Input"].cells.last_cell.row)).end('up').row)
+    # Acquire the input data
+    last_input_row = min(260, wb.sheets["Input"].range("B" + str(wb.sheets["Input"].cells.last_cell.row)).end('up').row)
     if last_input_row > 9:
         input_range = wb.sheets["Input"].range("B10:F" + str(last_input_row)).value
 
@@ -84,8 +75,14 @@ def add_cards(sheet):
         error_list = []
         error_row_list = []
         count = 1
+
+        # For each input
         for element in input_range:
+            
+            # Make its master name
             master_name = build_master_name(element)
+            
+            # If there is an error, save it for an alert later
             if master_name == "Error":
                 error_list.append(element[0])
                 error_row_list.append(count)
@@ -94,23 +91,33 @@ def add_cards(sheet):
             
             count += 1
 
-        formulas = sheet.range("a3:v3").copy()
+        # Copy the formulas into the next row
+        formulas = sheet.range("a2:x2").copy()
         next_sheet_row = last_row(sheet) + 1
         end_input_row = next_sheet_row + len(input_range) - 1 - len(error_list)
-        sheet.range("a" + str(next_sheet_row) + ":v" + str(end_input_row)).paste(paste="formulas_and_number_formats")
+        sheet.range("a" + str(next_sheet_row) + ":x" + str(end_input_row)).paste(paste="formulas_and_number_formats")
+        
+        # Set the B column (names) to the input list
         sheet.range("b" + str(next_sheet_row)).options(ndim=2).value = input_list
 
+        # Issue message box error alerts for any input errors (hopefully none sneak through)
         for idx in range(len(error_list)):
             win32api.MessageBox(wb.app.hwnd, "Input error: " + error_list[idx] + ", row " + str(error_row_list[idx] + 9), "Failure - Input Error")
 
-def remove_cards(sheet):
+def remove_cards(sheet, wb):
+    '''Removes the cards listed in the entry field from a sheet if they are found.
+    sheet: the wb.sheets["name"] sheet that will be manipulated
+    wb: the workbook object
+    '''
 
+    # Check for erroneous entries and alert
     error_check = wb.sheets["Input"].range("f9").value.split(" ")[2].strip()
     if int(error_check) > 0:
         win32api.MessageBox(wb.app.hwnd, "Errors have been detected in your input! Please check that the card is spelled correctly and that you have assembled a correct " + 
             "combination of set, style, and foiling.\n\nYou can use the Card Search function for additional help.", "Input Error Detected")
         return
 
+    # Save the input data
     last_input_row = min(250, wb.sheets["Input"].range("B" + str(wb.sheets["Input"].cells.last_cell.row)).end('up').row)
     if last_input_row > 9:
         input_range = wb.sheets["Input"].range("B10:F" + str(last_input_row)).value
@@ -127,42 +134,51 @@ def remove_cards(sheet):
             else:
                 input_list.append(master_name)
             count += 1
-        
+
+        # Pull the cardnames from the sheet        
         last_data_row = last_row(sheet)
         cardnames = sheet.range("b4:b" + str(last_data_row)).value
 
+        # Loop over the cards to remove and the cardname list and store the row numbers to be removed
         rows_to_remove = []
         failed_searches = []
         for element in input_list:
             current_row = 4
             card_found = False
             for name in cardnames:
-                if name.lower() == element.lower():
-                    card_found = True
-                    rows_to_remove.append(current_row)
-                current_row += 1
+                try:
+                    if name.lower() == element.lower():
+                        card_found = True
+                        rows_to_remove.append(current_row)
+                    current_row += 1
+                except:
+                    current_row += 1
             if card_found == False:
                 failed_searches.append(element)
         
+        # Run through the rows that need to be removed and remove them
         rows_removed = 0
         for row in rows_to_remove:
             sheet.range(str(row - rows_removed) + ":" + str(row - rows_removed)).delete()
             rows_removed += 1
 
+        # Send out error alerts
         for idx in range(len(error_list)):
             win32api.MessageBox(wb.app.hwnd, "Input error: " + error_list[idx] + ", row " + str(error_row_list[idx] + 9), "Failure - Input Error")    
 
         for remnant in failed_searches:
             win32api.MessageBox(wb.app.hwnd, "Card not found error: " + remnant + " not found in " + sheet.name, "Failure - Card Not Found")
 
-def track_prices():
+def track_prices(wb):
 
+    # Grab the entire price history in the sheet and move it over two columns
     price_tracking = wb.sheets["Price Tracking"]
     ptcolumn = last_column(price_tracking)
     ptrow = last_row(price_tracking)
     old_prices = price_tracking.range("a1", (ptrow, ptcolumn)).copy()
     price_tracking.range("c1").paste()
 
+    # Get the most up to date prices from the pricing sheet and copy it over
     prices = wb.sheets["Pricing"]
     prow = last_row(prices)
     ids = prices.range("a1:a" + str(prow)).copy()
@@ -170,13 +186,76 @@ def track_prices():
     new_prices = prices.range("k1:k" + str(prow)).copy()
     price_tracking.range("b1").paste(paste="values")
 
+    # Add the date so the system knows what it is looking at
     prices.range("a1").value = datetime.now().strftime("%m/%d/%Y")
+
+'''
+These methods are designed to be called from macros in VBA.
+'''
+def update_system():
+    '''
+    Updates the cardlist and pricing of the system.
+    '''
+    # Set the workbook
+    wb = xw.Book.caller()
+
+    # Update price history
+    track_prices(wb)
+
+    # Update cards and prices (takes about 3 minutes)
+    cards, card_count = download_bulk_data()
+    prices = get_tcg_pricing(cards, card_count)
+
+    # Create the dataframes 
+    cards_df = pandas.DataFrame.from_dict(cards, orient="index")
+    cards_df = cards_df.sort_values(by=['cardname', 'set_name', 'style_type', 'foil_type'])
+
+    prices_df = pandas.DataFrame.from_dict(prices, orient="index")
+    prices_df = prices_df.sort_index()
+
+    # These actions refer to named ranges
+    # Update the data in the spreadsheet
+    xw.Range("Cards").clear()
+    xw.Range("Cards").value = cards_df
+
+    xw.Range("CurrentPrices").clear()
+    xw.Range("CurrentPrices").value = prices_df
     
+    # Update the spreadsheet most recent date
+    wb.sheets["Pricing"].range("a1").value = datetime.now().strftime("%m/%d/%Y")
 
+def eternal_add():
 
-# add_cards(wb.sheets["Eternal Case"])
-# remove_cards(wb.sheets["Eternal Case"])
-track_prices()
+    wb = xw.Book.caller()
+    sheet = wb.sheets["Eternal Case"]
+    add_cards(sheet, wb)
 
-wb.save()
-    
+def standard_add():
+
+    wb = xw.Book.caller()
+    sheet = wb.sheets["Standard Case"]
+    add_cards(sheet, wb)
+
+def new_set_add():
+
+    wb = xw.Book.caller()
+    sheet = wb.sheets["New Set Case"]
+    add_cards(sheet, wb)
+
+def eternal_remove():
+
+    wb = xw.Book.caller()
+    sheet = wb.sheets["Eternal Case"]
+    remove_cards(sheet, wb)
+
+def standard_remove():
+
+    wb = xw.Book.caller()
+    sheet = wb.sheets["Standard Case"]
+    remove_cards(sheet, wb)
+
+def new_set_remove():
+
+    wb = xw.Book.caller()
+    sheet = wb.sheets["New Set Case"]
+    remove_cards(sheet, wb)
